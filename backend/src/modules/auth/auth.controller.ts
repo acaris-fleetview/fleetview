b5d8bdc0061f09994e2bd7d3afbdd58112122d5b
acaris-fleetview <sebastien@acaris.Fr>
2026-05-29 09:37:06 +0200

    Update auth.controller.ts

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -4,19 +4,32 @@ import { AuthService } from './auth.service';
 import { IsEmail, IsString } from 'class-validator';
 
 class LoginDto {
-  @IsEmail() email: string;
-  @IsString() password: string;
+    @IsEmail() email: string;
+    @IsString() password: string;
+}
+
+class RegisterDto {
+    @IsEmail() email: string;
+    @IsString() password: string;
+    @IsString() name: string;
 }
 
 @ApiTags('Auth')
-@Controller('auth')
-export class AuthController {
-  constructor(private auth: AuthService) {}
+  @Controller('auth')
+  export class AuthController {
+    constructor(private auth: AuthService) {}
 
   @Post('login')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Connexion utilisateur' })
-  login(@Body() dto: LoginDto) {
-    return this.auth.login(dto.email, dto.password);
-  }
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Connexion utilisateur' })
+    login(@Body() dto: LoginDto) {
+          return this.auth.login(dto.email, dto.password);
+    }
+
+  @Post('register')
+    @HttpCode(201)
+    @ApiOperation({ summary: 'Créer un utilisateur' })
+    register(@Body() dto: RegisterDto) {
+          return this.auth.register(dto.email, dto.password, dto.name);
+    }
 }
